54e74835335840f1e54232aaa286a5696552eabb
Hayeon <khy920723@genieworks.net>
2022-06-30 17:05:41 +0900

    mavenTest not complete 20220630

---
+++ src/main/java/com/khy3/maventest/MavenTestApplication.java
@@ -0,0 +1,13 @@
+package com.khy3.maventest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khy3/maventest/MavenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khy3.maventest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
